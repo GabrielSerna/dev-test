42582edf9f07868adba035e54eb81ea6f30fd510
capocolo <gabrielserna.hce@gmail.com>
2020-08-31 17:45:41 +0200

    00_note

---
+++ 03_javascript/01/script.js
@@ -0,0 +1,23 @@
+/*  WHILE
+count = 0
+while (count < 5) {
+    console.log("contatore: " + count)
+    count++
+}
+*/
+
+
+/* DO-WHILE
+count = 0;
+do {
+    console.log(count + " * 7 = " + count * 7);
+} while (count++ < 10);
+*/
+
+
+/* FOR
+for (count = 0 ; count <= 10 ; count++) {           
+    console.log(count + " * 7 = " + count * 7);
+}
+*/
+
