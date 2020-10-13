d379ca280b19b3d84532731a278beb1d2e3cd667
capocolo <gabrielserna.hce@gmail.com>
2020-10-13 18:02:41 +0200

    2020 10 13

--- 05_csvInJson/file.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-fs.readFile('./05_csvInJson/data01.csv', (err, data) => {
-  if (err) {
-    console.error(err);
-  return
-  }
-  console.log(`i dati sono passati`);
-});
