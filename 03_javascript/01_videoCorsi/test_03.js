650cf904295feb324ddc5f4a16acf082be1b2a51
capocolo <gabrielserna.hce@gmail.com>
2020-10-29 17:20:03 +0100

    2020 10 29

---
+++ 03_javascript/01_videoCorsi/test_03.js
@@ -0,0 +1,12 @@
+let arrNum = [1, 2, 3, 4, 5, 6, 7];
+let arrNom = ['Gabriel', 'Mattia', 'Riccardo', 'Federica'];
+
+if (arrNum.length > 5 && arrNom.length === 4) {
+  console.log(`è tutto vero!!!!!!!!!!!`);
+} else { `no no no è falso`};
+
+let people = 6;
+let tempo = 'sole';
+let prenotation = 1;
+
+// if ( day == )
