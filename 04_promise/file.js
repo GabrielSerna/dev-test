c2bfa5dd341098ac7cec0e45fcaf7e80a7de58f2
capocolo <gabrielserna.hce@gmail.com>
2020-12-03 11:59:53 +0100

    2020 12 03

--- 04_promise/file.js
+++ 04_promise/file.js
@@ -62,3 +62,24 @@ function ex03() {
 };
 
 //==============================================================================
+function ex04() {
+  function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+
+  asyncCall();
+
+}
+ex04();
+//==============================================================================
