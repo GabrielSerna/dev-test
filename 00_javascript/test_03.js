7469d4751cc458f509ded7d1224dd2ba119c28ee
capocolo <gabrielserna.hce@gmail.com>
2021-01-29 14:34:42 +0100

    2021 01 29

--- 00_javascript/test_03.js
+++ 00_javascript/test_03.js
@@ -1,14 +1,32 @@
-let course =
-  {
+let course = {
     _id: "ddd00000000000000ba00001",
     active: true,
     modules: [
           {
               module_id: "ddd00000000000000ca00001",
-              active: true,
-              section: "[01]Introduzione al Master",
-              title: "Benvenuto al Master Level 1",
               order: 1,
-          },        
+          },
+          {
+              module_id: "ddd00000000000000ca00005",
+              order: 5,
+          },
+          {
+            order: 7,
+            module_id: "ddd00000000000000ca00007",
+          },
+          {
+              module_id: "ddd00000000000000ca00003",
+              order: 3,
+          },
+          {
+              module_id: "ddd00000000000000ca00010",
+              order: 10,
+          }
         ]
   }
+
+let mod = course.modules;
+
+let newMod = mod.map( mod.find( ))
+
+console.log(mod);
