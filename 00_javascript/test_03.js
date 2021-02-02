7c193b8172a09046a323ce324b58e3c279745ecc
capocolo <gabrielserna.hce@gmail.com>
2021-02-02 11:31:31 +0100

    test

--- 00_javascript/test_03.js
+++ 00_javascript/test_03.js
@@ -12,8 +12,8 @@ let courses = {
       order: 5,
     },
     {
-      order: 7,
       module_id: "ddd00000000000000ca00007",
+      order: 7,
     },
     {
       module_id: "ddd00000000000000ca00003",
@@ -33,46 +33,38 @@ let newModule = [
   }
 ]
 
-// let mod = courses.moduleNumber;
-// let order = mod.sort();
-// let newModNum = mod.filter(el => el !== 1)
-// console.log(newModNum);
+let modules = courses.modules;
 
-function compare(a, b) {
-  const moduleA = a.order;
-  const moduleB = b.order;
-
-  let comparison = 0;
-
-  if (moduleA > moduleB) {
-    comparison = 1;
-  } else if (moduleA < moduleB) {
-    comparison = -1;
-  }
-  return comparison;
-}
-
-
-export const reorderAll = (unordered, newEl, prop, renum) => {
-
-
-
-
-
-
-}
-
-let newOrder1 = courses.modules.sort(compare);
-console.log(newOrder1);
-
-// const totalModules = ( modules, module, order) => {
-  
-//   let addModule = [...modules, module];
-  
-//   let newOrder = addModule.sort(order);
-
-//   console.log(newOrder);
-  
+// METODO 1
+// function compare(a, b) {
+//   const moduleA = a.order;
+//   const moduleB = b.order;
+//   let comparison = 0;
+//   if (moduleA > moduleB) {
+//     comparison = 1;
+//   } else if (moduleA < moduleB) {
+//     comparison = -1;
+//   }
+//   return comparison;
 // }
-  
-// totalModules(course.modules, newModule, compare());
+
+// METODO 2
+function compare(a, b) {
+  a.order - b.order;
+  }
+// console.log(modules.sort(compare));
+
+
+const reOrderAll = (unordered, newEl, prop, renum) => {
+
+let allEl = [...unordered, newEl];
+
+reorder = (a, b) => {
+  a.order - b.order;
+} 
+
+console.log(allEl.sort(reorder));
+
+}
+
+reOrderAll(modules, newModule);
