caf52f497d88efceff6e5bdd8bec431f37f1771b
capocolo <gabrielserna.hce@gmail.com>
2021-06-01 22:24:06 +0200

    test

--- videocorsi/theCompleteDevelopersGuideToMongoDb/test/test_helper.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
